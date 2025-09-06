8b23cf7c2d99f4a57d052fb2e00ea3d4a8ec056d
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-06 12:33:13 +0800

    Add entity fo UC1

---
+++ src/ShelfMarket.Domain/Entities/Shelf.cs
@@ -0,0 +1,17 @@
+﻿namespace ShelfMarket.Domain.Entities;
+
+public class Shelf
+{
+    public Guid? Id { get; set; }
+    public int Number { get; set; }
+    public Guid ShelfTypeId { get; set; }
+
+    public Shelf()
+    { }
+
+    public Shelf(int number, Guid shelfTypeId)
+    {
+        Number = number;
+        ShelfTypeId = shelfTypeId;
+    }
+}
