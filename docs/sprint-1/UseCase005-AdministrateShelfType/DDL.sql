20958c1507c135d2bc24abc436df25d9defa9a45
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-06 13:54:13 +0800

    Add DDL sql  for UC5

---
+++ docs/sprint-1/UseCase005-AdministrateShelfType/DDL.sql
@@ -0,0 +1,21 @@
+﻿-- Create database if it does not exist
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'ShelfMarked_dev')
+BEGIN
+    CREATE DATABASE ShelfMarked_Dev;
+END
+GO
+
+-- Use the database
+USE ShelfMarked_Dev;
+GO
+
+-- Create table if it does not exist
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'SHELFTYPE')
+BEGIN
+    CREATE TABLE SHELFTYPE (
+        Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+        Name NVARCHAR(255) NOT NULL,
+        Description NVARCHAR(200) NULL
+    );
+END
+GO
