5cd214e21406b8313ce7e09e7bc2931558447459
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-06 12:28:32 +0800

    Add Entity for UC5

---
+++ src/ShelfMarket.Domain/Entities/ShelfType.cs
@@ -0,0 +1,18 @@
+﻿namespace ShelfMarket.Domain.Entities;
+
+public class ShelfType
+{
+    public Guid? Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+
+    public ShelfType()
+    {
+    }
+
+    public ShelfType(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
