c312c91b66d359af099f480779ac3fe756e26db5
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-11 13:45:29 +0800

    Add status bar

---
+++ src/ShelfMarket.UI/ViewModels/StatusBarViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ShelfMarket.UI.ViewModels.Abstracts;
+
+namespace ShelfMarket.UI.ViewModels;
+
+public class StatusBarViewModel : ModelBase
+{
+    private string _user;
+
+    public string User
+    {
+        get { return _user; }
+        set { _user = value; OnPropertyChanged(); }
+    }
+
+    private bool _isDbConnected;
+
+    public bool IsDbConnected
+    {
+        get { return _isDbConnected; }
+        set
+        {
+            _isDbConnected = value;
+            OnPropertyChanged(nameof(IsDbNotConnected));
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsDbNotConnected => !IsDbConnected;
+
+    private bool _isDbSaving;
+
+    public bool IsDbSaving
+    {
+        get { return _isDbSaving; }
+        set { _isDbSaving = value; OnPropertyChanged(); }
+    }
+
+
+
+    public StatusBarViewModel()
+    {
+        User = string.Empty;
+        IsDbConnected = false;
+        IsDbSaving = false;
+    }
+}
