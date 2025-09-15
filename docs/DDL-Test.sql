66f154d0df274d88b9585a87cda08ff88124aa68
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-15 13:43:53 +0800

    Update SQL for Test Db to use new talbe names

--- docs/DDL-Test.sql
+++ docs/DDL-Test.sql
@@ -1,18 +1,22 @@
-﻿-- Create database if it does not exist
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'ShelfMarked_Test')
+﻿---------------------------------------------------------
+--------------             DDL             --------------
+---------------------------------------------------------
+
+-- Create database if it does not exist
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'ShelfMarket_Test')
 BEGIN
-    CREATE DATABASE ShelfMarked_Test;
+    CREATE DATABASE [ShelfMarket_Test];
 END
 GO
 
 -- Use the database
-USE ShelfMarked_Test;
+USE [ShelfMarket_Test];
 GO
 
 -- Create table if it does not exist
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'SHELFTYPE')
 BEGIN
-    CREATE TABLE SHELFTYPE (
+    CREATE TABLE dbo.SHELFTYPE (
         Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
         Name NVARCHAR(255) NOT NULL UNIQUE,
         Description NVARCHAR(200) NULL
@@ -23,12 +27,45 @@ GO
 -- Create SHELF table if it does not exist
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'SHELF')
 BEGIN
-    CREATE TABLE SHELF (
+    CREATE TABLE dbo.SHELF (
         Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
         Number INT NOT NULL UNIQUE,
         ShelfTypeId UNIQUEIDENTIFIER NOT NULL,
+        LocationX INT NOT NULL,
+        LocationY INT NOT NULL,
         CONSTRAINT FK_SHELF_SHELFTTYPE FOREIGN KEY (ShelfTypeId)
-            REFERENCES SHELFTTYPE(Id)
+            REFERENCES dbo.SHELFTYPE(Id),
+            UNIQUE (LocationX, LocationY)
+
+    );
+END
+GO
+
+-- Create table if it does not exist
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'SHELFRENTCONTRACT' AND TABLE_SCHEMA = N'dbo')
+BEGIN
+    CREATE TABLE dbo.SHELFRENTCONTRACT (
+        Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+        ShelfTenantId UNIQUEIDENTIFIER NOT NULL,
+        ContractNumber INT NOT NULL UNIQUE,
+        StartDate DATE NOT NULL,
+        EndDate DATE NOT NULL,
+        Cancelled DATE NULL
+    );
+END
+GO
+
+-- Create table if it does not exist
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'SHELFRENTCONTRACTLINE' AND TABLE_SCHEMA = N'dbo')
+BEGIN
+    CREATE TABLE dbo.SHELFRENTCONTRACTLINE (
+        Id UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+        ShelfRentContractId UNIQUEIDENTIFIER NOT NULL,
+        LineNumber INT NOT NULL,
+        Description NVARCHAR(255) NOT NULL,
+        Amount DECIMAL(18, 2) NOT NULL,
+        CONSTRAINT UQ_ShelfRentContract_LineNumber UNIQUE (ShelfRentContractId, LineNumber),
+        CONSTRAINT FK_ShelfRentContract FOREIGN KEY (ShelfRentContractId) REFERENCES dbo.SHELFRENTCONTRACT(Id)
     );
 END
 GO
