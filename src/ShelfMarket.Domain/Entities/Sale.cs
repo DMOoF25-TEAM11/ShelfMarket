3b07cf34f96a06093ac9d08b2fd32376b6b78eed
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-09-17 12:38:04 +0800

    Add Entity for UseCase004

---
+++ src/ShelfMarket.Domain/Entities/Sale.cs
@@ -0,0 +1,22 @@
+﻿using ShelfMarket.Domain.ValueObjects;
+
+namespace ShelfMarket.Domain.Entities;
+
+public class Sale
+{
+    public Guid? Id { get; set; }
+    public uint? Number { get; set; }
+    public DateTime Date { get; set; }
+    public PaymentMetode PaymentMetode { get; set; }
+
+    public Sale()
+    {
+
+    }
+
+    public Sale(PaymentMetode paymentMetode = PaymentMetode.Cash)
+    {
+        Date = DateTime.Now;
+        PaymentMetode = paymentMetode;
+    }
+}
